5f3d2fde7aa8d008cbfa3684caf3413977bc358c
taedigit <taedigit@gmail.com>
2025-06-17 23:45:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ReactStudyWeb/',  // 이 줄 추가
+  base: '/ReactStudyWeb/', 
   plugins: [react()],
   server: {
     port: 3000,
